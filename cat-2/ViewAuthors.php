f959f72358a1d3cf1762f023c20b220695c00c35
RazaMu <45406532+RazaMu@users.noreply.github.com>
2023-11-18 10:04:38 +0300

    Add files via upload

--- cat-2/ViewAuthors.php
+++ cat-2/ViewAuthors.php
@@ -2,18 +2,18 @@
 // ViewAuthors.php
 
 // Include database connection file
-require_once 'DbConn.php'; // Adjust the path as necessary
+require_once 'configs/DbConn.php'; // Adjust the path as necessary
 
 try {
     // Prepare an SQL statement to select all authors
     $stmt = $DbConn->prepare("SELECT * FROM authorstb ORDER BY AuthorFullName ASC");
-    
+
     // Execute the prepared statement
     $stmt->execute();
 
     // Set the resulting array to associative
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    
+
     // Start the HTML content
     echo "<!DOCTYPE html>
     <html>
@@ -32,10 +32,11 @@ try {
             <th>Biography</th>
             <th>Date of Birth</th>
             <th>Suspended</th>
+            <th>Action</th> <!-- New column for Edit and Delete links/buttons -->
         </tr>";
 
     // Fetching each row as an associative array and displaying it
-    foreach($stmt->fetchAll() as $author) {
+    foreach ($stmt->fetchAll() as $author) {
         echo "<tr>
                 <td>" . $author['AuthorId'] . "</td>
                 <td>" . htmlspecialchars($author['AuthorFullName']) . "</td>
@@ -44,13 +45,18 @@ try {
                 <td>" . htmlspecialchars($author['AuthorBiography']) . "</td>
                 <td>" . htmlspecialchars($author['AuthorDateOfBirth']) . "</td>
                 <td>" . ($author['AuthorSuspended'] ? 'Yes' : 'No') . "</td>
+                <td>
+                    <a href='editAuthor.php?id=" . $author['AuthorId'] . "'>Edit</a>
+                    |
+                    <a href='deleteAuthor.php?id=" . $author['AuthorId'] . "'>Delete</a>
+                </td>
               </tr>";
     }
 
     echo "</table>
     </body>
     </html>";
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
 ?>
